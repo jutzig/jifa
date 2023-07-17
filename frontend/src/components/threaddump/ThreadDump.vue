019f3fd25e07e0bad48058d9f6816a42b36083ff
Johannes Utzig <jutzig.dev@gmail.com>
2023-07-17 23:01:10 +0200

    fixed layout in ThreadDump
    -get rid of extra-scrollbars with overflow: auto
    -use el-aside for navbar in threaddump

--- frontend/src/components/threaddump/ThreadDump.vue
+++ frontend/src/components/threaddump/ThreadDump.vue
@@ -12,52 +12,55 @@
  -->
 
 <template>
-  <el-container style="height: 100%">
-    <el-header>
-      <view-menu subject="analysisResult"
-                 :file="file"
-                 :analysisState="analysisState"
-                 type="THREAD_DUMP"/>
-    </el-header>
-
-    <el-main style="padding-top: 0">
-
-      <div style="padding-top: 20px" v-if="analysisState === 'IN_PROGRESS' || analysisState === 'ERROR'">
-        <b-progress height="2rem" show-progress :precision="2"
-                    :value="progress"
-                    :variant="progressState"
-                    striped
-                    :animated="progress < 100"></b-progress>
-        <b-card class="mt-3" bg-variant="dark" text-variant="white" v-if="message">
-          <b-card-text style="white-space: pre-line;">{{ message }}</b-card-text>
-          <div class="d-flex justify-content-center mb-3" v-if="progressState === 'info'">
-            <b-spinner></b-spinner>
+  <el-container  style="height: 100%">
+      <el-container>
+        <el-header>
+          <view-menu subject="analysisResult"
+                     :file="file"
+                     :analysisState="analysisState"
+                     type="THREAD_DUMP"/>
+        </el-header>
+    
+        <el-main style="padding-top: 0">
+    
+          <div style="padding-top: 20px" v-if="analysisState === 'IN_PROGRESS' || analysisState === 'ERROR'">
+            <b-progress height="2rem" show-progress :precision="2"
+                        :value="progress"
+                        :variant="progressState"
+                        striped
+                        :animated="progress < 100"></b-progress>
+            <b-card class="mt-3" bg-variant="dark" text-variant="white" v-if="message">
+              <b-card-text style="white-space: pre-line;">{{ message }}</b-card-text>
+              <div class="d-flex justify-content-center mb-3" v-if="progressState === 'info'">
+                <b-spinner></b-spinner>
+              </div>
+            </b-card>
           </div>
-        </b-card>
-      </div>
-
-      <el-container v-if="analysisState === 'SUCCESS'" style="height: 100%">
-        <div style="left: 0%;top: 60px; position: fixed; z-index: 100; width: 10%; height: 100%; font-size: 16px">
-          <el-card :header="$t('jifa.threadDump.navigation')" style="height: 100%;">
-            <div class="nav-item"><a href="#navTop">{{ $t('jifa.threadDump.navToTop') }}</a></div>
-            <el-divider/>
-            <div class="nav-item"><a href="#overview">{{ $t('jifa.threadDump.basicInfo') }}</a></div>
-            <div class="nav-item"><a href="#blockedThreads">{{ $t('jifa.threadDump.blockedThreadsLabel') }}</a></div>
-            <div class="nav-item"><a href="#threadSummary">{{ $t('jifa.threadDump.threadSummary') }}</a></div>
-            <div class="nav-item"><a href="#threadGroupSummary">{{ $t('jifa.threadDump.threadGroupSummary') }}</a></div>
-            <div class="nav-item"><a href="#monitors">{{ $t('jifa.threadDump.monitors') }}</a></div>
-            <div class="nav-item"><a href="#callSiteTree">{{ $t('jifa.threadDump.callSiteTree') }}</a></div>
-            <el-divider/>
-            <div class="nav-item"><a href="#fileContent" @click="showDetail('fileContent')">{{ $t('jifa.threadDump.fileContent') }}</a></div>
-          </el-card>
-        </div>
-      
-        <el-main style="padding: 5px; height: 100%;">
-          <main-page :file="file" style="width: 90%; left: 10%;" id="navTop"/>
+    
+          <el-container v-if="analysisState === 'SUCCESS'" style="height: 100%">
+            <el-aside width="200px">
+              <div style="font-size: 16px; height: 100%;">
+              <el-card :header="$t('jifa.threadDump.navigation')" style="height: 100%;">
+                <div class="nav-item"><a href="#navTop">{{ $t('jifa.threadDump.navToTop') }}</a></div>
+                <el-divider/>
+                <div class="nav-item"><a href="#overview">{{ $t('jifa.threadDump.basicInfo') }}</a></div>
+                <div class="nav-item"><a href="#blockedThreads">{{ $t('jifa.threadDump.blockedThreadsLabel') }}</a></div>
+                <div class="nav-item"><a href="#threadSummary">{{ $t('jifa.threadDump.threadSummary') }}</a></div>
+                <div class="nav-item"><a href="#threadGroupSummary">{{ $t('jifa.threadDump.threadGroupSummary') }}</a></div>
+                <div class="nav-item"><a href="#monitors">{{ $t('jifa.threadDump.monitors') }}</a></div>
+                <div class="nav-item"><a href="#callSiteTree">{{ $t('jifa.threadDump.callSiteTree') }}</a></div>
+                <el-divider/>
+                <div class="nav-item"><a href="#fileContent" @click="showDetail('fileContent')">{{ $t('jifa.threadDump.fileContent') }}</a></div>
+              </el-card>
+            </div>
+           </el-aside>
+            <el-main style="padding: 5px; height: 100%;">
+              <main-page :file="file" style="width: 90%; left: 10%;" id="navTop"/>
+            </el-main>
+          </el-container>
         </el-main>
       </el-container>
-    </el-main>
-  </el-container>
+  </el-container> 
 </template>
 
 <script>
